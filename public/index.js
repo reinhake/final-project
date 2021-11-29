dce3da83328275c0f3cc5c056c84be08ed146a02
reinhake <reinhake@oregonstate.edu>
2021-11-28 23:30:41 -0800

    Changed the Look and added gitignore

--- public/index.js
+++ public/index.js
@@ -1,88 +1,140 @@
+
+
 var current = document.querySelector('.mon')
+var curb = document.querySelector('.dropbtn')
 var triceps = document.querySelector('.tricep')
 var biceps = document.querySelector('.bicep')
 var legs = document.querySelector('.leg')
+var should = document.querySelector('.should')
+var chests = document.querySelector('.chest')
+var backs = document.querySelector('.back')
+var ab = document.querySelector('.ab')
+var card = document.querySelector('.card')
 
 
 
-function getGyms () {
-	var req = new XMLHttpRequest();
-	var reqURL = "/gyms";
-	req.open('GET', reqURL);
-	req.addEventListener('load', function (eventt) {
-		if (eventt.target.status == 200){
-			res = req.responseText
-			console.log(req.response)
-			return JSON.parse(res)
-		}
-	})
-}
 
 function userAction() {
 	url = "https://limitless-woodland-36659.herokuapp.com/https://wiki-image.herokuapp.com/getFirstImage?WikiUrl=https://en.wikipedia.org/wiki/"
 	bicep = url + "Biceps_curl"
 	tricep = url + "Triceps"
 	leg = url + "List_of_weight_training_exercises"
-	exercise = url + "Exercise"
+	shoulder = url + "Pull-down_(exercise)"
+	chest = url + "Dumbbell"
+	back = url + "Hyperextension_(exercise)"
+	abs = url + "Crunch_(exercise)"
+	cardio = url + "Aerobic_exercise"
 	add = document.getElementsByClassName('exercise-src')
 	gen = document.getElementsByClassName('generic')
 	
 	fetch(bicep)
 		.then(res => res.json())
 		.then(result => {
-//			console.log(result)
 			add[0].src = result.firstImage.src
 		})
 		.catch(err=>console.log(err))
 	fetch(tricep)
 		.then(res => res.json())
 		.then(result => {
-//			console.log(result)
 			add[1].src = result.firstImage.src
 		})
 		.catch(err=>console.log(err))
 	fetch(leg)
 		.then(res => res.json())
 		.then(result => {
-//			console.log(result)
 			add[2].src = result.firstImage.src
 		})
 		.catch(err=>console.log(err))
-	fetch(exercise)
+	fetch(shoulder)
 		.then(res => res.json())
 		.then(result => {
-//			console.log(result)
-			gen[0].src = result.firstImage.src
+			add[3].src = result.firstImage.src
+		})
+		.catch(err=>console.log(err))
+	fetch(chest)
+		.then(res => res.json())
+		.then(result => {
+			add[4].src = result.firstImage.src
+		})
+		.catch(err=>console.log(err))
+	fetch(back)
+		.then(res => res.json())
+		.then(result => {
+			add[5].src = result.firstImage.src
+		})
+		.catch(err=>console.log(err))
+	fetch(abs)
+		.then(res => res.json())
+		.then(result => {
+			add[6].src = result.firstImage.src
+		})
+		.catch(err=>console.log(err))
+	fetch(cardio)
+		.then(res => res.json())
+		.then(result => {
+			add[7].src = result.firstImage.src
 		})
 		.catch(err=>console.log(err))
 }
 
-var selector = document.querySelector('#day')
 
-selector.addEventListener("click", function () {
-	if(selector.selectedIndex == 0){
-		current = document.querySelector(".mon")
-	}
-	else if(selector.selectedIndex == 1){
-		current = document.querySelector(".tue")
-	}
-	else if(selector.selectedIndex == 2){
-		current = document.querySelector(".wed")
-	}
-	else if(selector.selectedIndex == 3){
-		current = document.querySelector(".thu")
-	}
-	else if(selector.selectedIndex == 4){
-		current = document.querySelector(".fri")
-	}
-	else if(selector.selectedIndex == 5){
-		current = document.querySelector(".sat")
-	}
-	else if(selector.selectedIndex == 6){
-		current = document.querySelector(".sun")
-	}
-
+var mon = document.querySelector('.Monday')
+mon.addEventListener("click", function () {
+	curb.innerText = "Monday"
+	current.classList.remove("focus")
+	current = document.querySelector(".mon")
+	current.classList.add("focus")
 })
+
+var tue = document.querySelector('.Tuesday')
+tue.addEventListener("click", function () {
+	curb.innerText = "Tuesday"
+	current.classList.remove("focus")
+	current = document.querySelector(".tue")
+	current.classList.add("focus")
+})
+
+var wed = document.querySelector('.Wednesday')
+wed.addEventListener("click", function () {
+	curb.innerText = "Wednesday"
+	current.classList.remove("focus")
+	current = document.querySelector(".wed")
+	current.classList.add("focus")
+})
+
+var thu = document.querySelector('.Thursday')
+thu.addEventListener("click", function () {
+	curb.innerText = "Thursday"
+	current.classList.remove("focus")
+	current = document.querySelector(".thu")
+	current.classList.add("focus")
+})
+
+var fri = document.querySelector('.Friday')
+fri.addEventListener("click", function () {
+	curb.innerText = "Friday"
+	current.classList.remove("focus")
+	current = document.querySelector(".fri")
+	current.classList.add("focus")
+})
+
+var sat = document.querySelector('.Saturday')
+sat.addEventListener("click", function () {
+	curb.innerText = "Saturday"
+	current.classList.remove("focus")
+	current = document.querySelector(".sat")
+	current.classList.add("focus")
+})
+
+var sun = document.querySelector('.Sunday')
+sun.addEventListener("click", function () {
+	curb.innerText = "Sunday"
+	current.classList.remove("focus")
+	current = document.querySelector(".sun")
+	current.classList.add("focus")
+})
+
+
 
 document.querySelectorAll(".exercise").forEach(item => {
 	item.addEventListener('click', function (){
@@ -96,6 +148,21 @@ document.querySelectorAll(".exercise").forEach(item => {
 			else if(item.classList[1] == 'legs'){
 				legs.appendChild(item)
 			}
+			else if(item.classList[1] == 'shoulds'){
+				should.appendChild(item)
+			}
+			else if(item.classList[1] == 'chests'){
+				chests.appendChild(item)
+			}
+			else if(item.classList[1] == 'backs'){
+				backs.appendChild(item)
+			}
+			else if(item.classList[1] == 'abs'){
+				ab.appendChild(item)
+			}
+			else if(item.classList[1] == 'cardio'){
+				card.appendChild(item)
+			}
 		}
 		else{
 			current.appendChild(item) 
@@ -103,32 +170,6 @@ document.querySelectorAll(".exercise").forEach(item => {
 		item.classList.toggle('change')
 	})
 })
-
-/*
-var dotw = document.querySelector('.day-exercise')
- {
-	if(selector.selectedIndex == 0){
-		dotw.innerText = "Monday"
-	}
-	else if(selector.selectedIndex == 1){
-		dotw.innerText = "Tuesday"
-	}
-	else if(selector.selectedIndex == 2){
-		dotw.innerText = "Wednesday"
-	}
-	else if(selector.selectedIndex == 3){
-		dotw.innerText = "Thursday"
-	}
-	else if(selector.selectedIndex == 4){
-		dotw.innerText = "Friday"
-	}
-	else if(selector.selectedIndex == 5){
-		dotw.innerText = "Saturday"
-	}
-	else if(selector.selectedIndex == 6){
-		dotw.innerText = "Sunday"
-	}
-})*/
 
 window.addEventListener("load", function(){
 	userAction()
